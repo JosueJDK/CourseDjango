f5f3639ae7629f991d49d1b8c314b1ea993e49e2
Josue Salazar <salazarjosue2604@gmail.com>
2022-06-30 09:02:48 -0500

    Ver. 01

--- courses/admin.py
+++ courses/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 
-from courses.models import Teacher
 from .models import Teacher, Course, HomeWork
 
 # Register your models here.
-admin.site.register(Teacher)
-admin.site.register(Course)
-admin.site.register(HomeWork)
+class TeacherAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Names',               {'fields': ['name']}),
+        ('Last Names', {'fields': ['patern_name', 'matern_name']}),
+    ]
+admin.site.register(Teacher, TeacherAdmin)
+
+class CourseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Teacher',               {'fields': ['teacher']}),
+        ('Course', {'fields': ['course_name','descripton']}),
+    ]
+admin.site.register(Course, CourseAdmin)
+
+class HomeWorkAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Course',               {'fields': ['course']}),
+        ('HomeWork', {'fields': ['homework_name', 'description']}),
+        ('Date Informations', {'fields': ['return_date']}),
+    ]
+admin.site.register(HomeWork, HomeWorkAdmin)
